d09ceb249fa29b2b5e8b308cd329b7083ff8ba92
Jackie92 <bydu007@163.com>
2018-03-10 22:04:56 +0800

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,32 +2,198 @@
   <div class="content">
     <h1>资料收集页</h1>
     <div class="form">
-      请填写本科院校<span class="red_star">*</span>
-      <input type="text" v-model="collageName">
-      选择专业<span class="red_star">*</span>
-      <select name="" id=""></select>
-      选择省份<span class="red_star">*</span>
-      <select name="" id=""></select>
-      选择报考学校<span class="red_star">*</span>
-      <select name="" id=""></select>
-      请填写姓名<span class="red_star">*</span>
-      <select name="" id=""></select>
-      请填写邮箱<span class="red_star">*</span>
-      <select name="" id=""></select>
-      请填写手机号<span class="red_star">*</span>
-      <select name="" id=""></select>
+      <div>
+        <p>请填写本科院校<span class="red_star">*</span></p>
+        <div class="enter">
+          <span class="fa fa-bank"></span>
+          <input type="text" v-model="collageName">
+        </div>
+      </div>
+      <div>
+        <p>选择专业<span class="red_star">*</span></p>
+        <div class="enter">
+          <span class="fa fa-graduation-cap"></span>
+          <select name="" id="" v-model="major">
+            <option value="cityplanning">城市规划</option>
+            <option value="architecture">建筑学</option>
+            <option value="landscape">风景园林</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <p>选择省份<span class="red_star">*</span></p>
+        <div class="enter">
+          <span class="fa fa-map-signs"></span>
+          <select name="" id="" v-model="province" @change="onProvinceChange">
+            <option value="北京">北京市</option>
+            <option value="浙江省">浙江省</option>
+            <option value="天津市">天津市</option>
+            <option value="安徽省">安徽省</option>
+            <option value="上海市">上海市</option>
+            <option value="福建省">福建省</option>
+            <option value="重庆市">重庆市</option>
+            <option value="江西省">江西省</option>
+            <option value="山东省">山东省</option>
+            <option value="河南省">河南省</option>
+            <option value="湖北省">湖北省</option>
+            <option value="湖南省">湖南省</option>
+            <option value="广东省">广东省</option>
+            <option value="海南省">海南省</option>
+            <option value="山西省">山西省</option>
+            <option value="青海省">青海省</option>
+            <option value="江苏省">江苏省</option>
+            <option value="辽宁省">辽宁省</option>
+            <option value="吉林省">吉林省</option>
+            <option value="台湾省">台湾省</option>
+            <option value="河北省">河北省</option>
+            <option value="贵州省">贵州省</option>
+            <option value="四川省">四川省</option>
+            <option value="云南省">云南省</option>
+            <option value="陕西省">陕西省</option>
+            <option value="甘肃省">甘肃省</option>
+            <option value="黑龙江省">黑龙江省</option>
+            <option value="香港特别行政区">香港特别行政区</option>
+            <option value="澳门特别行政区">澳门特别行政区</option>
+            <option value="广西壮族自治区">广西壮族自治区</option>
+            <option value="宁夏回族自治区">宁夏回族自治区</option>
+            <option value="新疆维吾尔自治区">新疆维吾尔自治区</option>
+            <option value="内蒙古自治区">内蒙古自治区</option>
+            <option value="西藏自治区">西藏自治区</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <p>选择报考学校<span class="red_star">*</span></p>
+        <div class="enter">
+          <span class="fa fa-bank"></span>
+          <select name="" id=""></select>
+        </div>
+      </div>
+      <div>
+        <p>请填写姓名<span class="red_star">*</span></p>
+        <div class="enter">
+          <span class="fa fa-child"></span>
+          <input name="" id="" />
+        </div>
+      </div>
+      <div>
+        <p>请填写邮箱<span class="red_star">*</span></p>
+        <div class="enter">
+          <span class="fa fa-envelope-o fa-fw"></span>
+          <input type="email" name="" id="">
+        </div>
+      </div>
+      <div>
+        <p>请填写住址<span class="red_star">*</span></p>
+        <v-distpicker @selected="onSelected"></v-distpicker>
+        <div class="enter">
+          <span class="fa fa-address-book-o"></span>
+          <input type="text">
+        </div>
+     </div>
+     <div>
+        <p>请填写手机号<span class="red_star">*</span></p>
+        <div class="enter">
+          <span class="fa fa-mobile"></span>
+          <input type="number" name="" id="">
+        </div>
+     </div>
+     
     </div>
   </div>
 </template>
 
 <script>
+import VDistpicker from 'v-distpicker'
+function ajax () {
+  var ajaxData = {
+    type: arguments[0].type || 'GET',
+    url: arguments[0].url || '',
+    async: arguments[0].async || 'true',
+    data: arguments[0].data || null,
+    dataType: arguments[0].dataType || 'text',
+    contentType: arguments[0].contentType || 'application/x-www-form-urlencoded',
+    beforeSend: arguments[0].beforeSend || function () {},
+    success: arguments[0].success || function () {},
+    error: arguments[0].error || function () {}
+  }
+  ajaxData.beforeSend()
+  var xhr = createxmlHttpRequest()
+  xhr.responseType = ajaxData.dataType
+  xhr.open(ajaxData.type, ajaxData.url, ajaxData.async)
+  xhr.setRequestHeader('Content-Type', ajaxData.contentType)
+  xhr.send(convertData(ajaxData.data))
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        ajaxData.success(xhr.response)
+      } else {
+        ajaxData.error()
+      }
+    }
+  }
+}
+
+function createxmlHttpRequest () {
+  if (window.ActiveXObject) {
+    // eslint-disable-next-line
+    return new ActiveXObject('Microsoft.XMLHTTP')
+  } else if (window.XMLHttpRequest) {
+    return new XMLHttpRequest()
+  }
+}
+
+function convertData (data) {
+  if (typeof data === 'object') {
+    var convertResult = ''
+    for (var c in data) {
+      convertResult += c + '=' + data[c] + '&'
+    }
+    convertResult = convertResult.substring(0, convertResult.length - 1)
+    return convertResult
+  } else {
+    return data
+  }
+}
 export default {
   name: 'index',
+  components: { VDistpicker },
   data () {
     return {
       msg: 'd',
-      collageName: ''
+      collageName: '',
+      major: '',
+      province: ''
     }
+  },
+  methods: {
+    onProvinceChange: function () {
+      console.log(this.province, this.major)
+      // let url = 'http://locateu.cn/tool/sql.class.php?mod=sql_province'
+      let url = './tool/sql.class.php?mod=sql_province'
+      let data = {
+        major: this.major,
+        Province: this.province
+      }
+      // ajax(url, data, method, success)
+      ajax({
+        type: 'POST',
+        url: url,
+        dataType: 'jsonp',
+        data: data,
+        success: function (msg) {
+          console.log(msg)
+        },
+        error: function () {
+          console.log('error')
+        }
+      })
+    },
+    onSelected: function (data) {
+      console.log(data)
+    }
+  },
+  mounted () {
   }
 }
 </script>
@@ -47,6 +213,9 @@ li {
 }
 a {
   color: #42b983;
+}
+p, span, input {
+  margin: 0;
 }
 .content {
   width: 98%;
@@ -76,13 +245,19 @@ a {
   font-size: 20px;
 }
 .content input, .content select {
-  display: block;
+  display: inline-block;
   margin-top: 3px;
   height: 40px;
   width: 90%;
   margin: 5px 0 0 0;
   padding: 0;
+  /* border-radius: 3px;
+  border: #AAB2BD solid 1px; */
+  border: none;
+}
+.enter {
   border-radius: 3px;
   border: #AAB2BD solid 1px;
+  padding: 0 5px;
 }
 </style>
